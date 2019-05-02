2ed33f100e059493616b09e24f893461cd0a02f4
Hasibul Hasan Chowdhury <hchowdhury404@gmail.com>
2019-05-02 16:49:08 +0600

    Add files via upload

---
+++ constant.php
@@ -0,0 +1,5 @@
+<?php
+//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
+define('SITE_KEY',"6LcNYqEUAAAAAMkAqv8xiW2WN_YH2sZEO05e-vhT"); 
+define('SECRET_KEY',"6LcNYqEUAAAAAHrxbQ8EzHbgefMrQJuoouBMCQFy");
+?>
